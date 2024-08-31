d82e211bdd9d2a1739049858a532ba68e0f809a5
Henri Kulmala <bhi259@myy.haaga-helia.fi>
2024-08-31 19:03:52 +0300

    Tehtävä 4 aloitus

---
+++ Viikkotehtäviä/Tehtava4/tehtava4/src/test/java/viikkotehtavat/tehtava4/Tehtava4ApplicationTests.java
@@ -0,0 +1,13 @@
+package viikkotehtavat.tehtava4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tehtava4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
